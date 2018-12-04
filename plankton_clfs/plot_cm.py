1c34d180706731decf981d6ccdc439847c232ccb
Mariane Valerio Gonzales (MSc | hirata) <mariane@marr.vision.ime.usp.br>
2018-12-04 17:29:25 -0200

    add cnn 79% acc

--- plankton_clfs/plot_cm.py
+++ plankton_clfs/plot_cm.py
@@ -1,40 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import gzip
 
-from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
-y_pred1 = np.load("./ndsb_dataset_nounk/predicted_labels_nounk.npy")
-#y_pred = np.argmax(y_pred1, axis=1)
+y_pred = np.load("./laps_nobg_100/predictions.npy")
+y_test = np.load("./laps_nobg_100/real_labels.npy")
 
-
-labels_path = "./ndsb_dataset_nounk/labels_train.npy.gz"
-with gzip.open(labels_path, "rb") as f:
-    y_test = np.load(f)
-#y_test1 = np.load("./laps_nobg_100/real_labels_tax.npy")
-
-valid_idx = np.load("./ndsb_dataset_nounk/indices_valid.npy")
-y_valid = y_test[valid_idx]
-
-print("Rótulos verdadeiros:\n", y_valid)
-print("Rótulos preditos:\n", y_pred1)
-
-"""
 def plot_confusion_matrix(cm,
                           normalize=False,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
-                        
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
      
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
-    tick_marks = np.arange((118))
+    tick_marks = np.arange((20))
     plt.xticks(tick_marks, rotation=45)
     plt.yticks(tick_marks)
+    """
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    """
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
@@ -50,7 +46,7 @@ plot_confusion_matrix(cnf_matrix, normalize=True,
 
 plt.show()
 
-# Compute barchart data
+# Compute barchart for classes
 classes_real, counts_reall = np.unique(y_test, return_counts=True)
 classes_pred, counts_pred = np.unique(y_pred, return_counts=True)
 
@@ -75,21 +71,21 @@ print("\nAccuracy:\n", total_accuracy)
 counts_real = np.copy(counts_reall)
 classes_real_sorted = np.zeros(len(classes_real), dtype='uint32')
 counts_real_sorted = np.zeros(len(counts_real), dtype='uint32')
-counts_pred_sorted = np.zeros(len(counts_real), dtype='uint32')
+counts_pred_sorted = np.zeros(len(counts_pred), dtype='uint32')
 mistakes_sorted = np.zeros(len(mistakes), dtype='uint32')
 percent_sorted = np.zeros(len(classes_real), dtype='float')
 for c in range(len(classes_real)):
     max_idx = np.argmax(counts_real)
     counts_real_sorted[-1-c] = counts_real[max_idx]
     classes_real_sorted[-1-c] = classes_real[max_idx]
-    #counts_pred_sorted[-1-c] = counts_pred[max_idx]
+    counts_pred_sorted[-1-c] = counts_pred[max_idx]
     mistakes_sorted[-1-c] = mistakes[max_idx]
     percent_sorted[-1-c] = percent[max_idx]
     counts_real[max_idx] = -1
 
 print("\nClasses sorted by number of images:\n", classes_real_sorted)
 print("\nSorted number of images in the validation set by class:\n", counts_real_sorted)
-#print("\nSorted number of images predicted in the validation set by class:\n", counts_pred_sorted)
+print("\nSorted number of images predicted in the validation set by class:\n", counts_pred_sorted)
 print("\nSorted number of mistakes in the validation set by class:\n", mistakes_sorted)
 print("\nSorted percent of mistakes in the validation set by class:\n", percent_sorted)
 
@@ -117,5 +113,7 @@ ax.set_xticklabels((classes_real_sorted))
 fig.tight_layout()
 plt.show()
 
-print(classification_report(y_test, y_pred))
-"""
+# TODO: Plot relation with focus measure
+
+
+
