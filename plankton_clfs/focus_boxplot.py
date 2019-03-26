2fe37a61ae975fbba6f91c4500504021f32d85b9
marivgonzales <marivgonzales@gmail.com>
2019-03-26 01:02:34 -0300

    adding graphs on focus and score analysis

--- plankton_clfs/focus_boxplot.py
+++ plankton_clfs/focus_boxplot.py
@@ -2,46 +2,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-predicted_labels = np.load("./ndsb_dataset_nounk/predicted_labels_079_valid.npy")
-real_labels = np.load("./ndsb_dataset_nounk/real_labels_nounk.npy")
 measures = np.load("focus_valid_ndsb_nounk.npy")
+predictions= np.load("./ndsb_dataset_nounk/complete_predictions_079_valid.npy")
 
-data = pd.DataFrame({'real':real_labels, 'predicted':predicted_labels, 'lapv':measures[:,0], 'lapm':measures[:,1], 'teng':measures[:,2]})
+#grouped
+predicted_labels = np.load("./ndsb_dataset_nounk/labels_predicted_079_valid_grouped.npy")
+real_labels = np.load("./ndsb_dataset_nounk/labels_real_079_valid_grouped.npy")
+scores = np.load("./ndsb_dataset_nounk/max_scores_predicted_079_valid_grouped.npy")
+measures = np.log(measures)
+
+data = pd.DataFrame({'max': scores, 'real':real_labels, 'predicted':predicted_labels, 'lapv':measures[:,0], 'lapm':measures[:,1], 'teng':measures[:,2]})
+#scores = pd.DataFrame(predictions)
+#data['max'] = scores.apply(max, axis=1)
 
 
+var = 'max'
+colors = ['blue', 'red']
 
-f, a = plt.subplots(1,80, sharex=False, sharey=True)
 
-for i in range(80):
-	box_true = data[data['predicted'] == i][data['real'] == i]['teng']
-	box_false = data[data['predicted'] != i][data['real'] == i]['teng']
-	a[i].boxplot([box_true, box_false], widths=0.8, labels=['+', '-'])
-	a[i].set_title(i)
-    
+#ploting boxplots
+f, a = plt.subplots(1,33, sharex=False, sharey=True)
+
+for i in range(33):
+	j = i
+	box_true = data[data['predicted'] == j][data['real'] == j][var]
+	box_false = data[data['predicted'] != j][data['real'] == j][var]
+	print("loading", j)
+	bplot = a[i].boxplot([box_true, box_false], widths=0.8, labels=['+', '-'],patch_artist=True)
+	a[i].set_title(j)
+
+	for patch, color in zip(bplot['boxes'], colors):
+		patch.set_facecolor(color)
+        
 f.subplots_adjust(hspace=0)
 plt.show()
-"""
-f1, a1 = plt.subplots(1,40, sharex=False, sharey=True)
 
-j=0
-for i in range(40, 80):
-	box_true = data[data['predicted'] == i][data['real'] == i]['teng']
-	box_false = data[data['predicted'] != i][data['real'] == i]['teng']
-	a1[j].boxplot([box_true, box_false], widths=0.8, labels=['+', '-'])
-	a1[j].set_title(i)
-	j = j+1
-    
-f1.subplots_adjust(hspace=0)
+"""
+f, a = plt.subplots(1,40, sharex=False, sharey=True)
+
+for i in range(40):
+	j = i
+	box_true = data[data['predicted'] == j][data['real'] == j][var]
+	box_false = data[data['predicted'] != j][data['real'] == j][var]
+	print("loading", j)
+	bplot = a[i].boxplot([box_true, box_false], widths=0.8, labels=['+', '-'],patch_artist=True)
+	a[i].set_title(j)
+
+	for patch, color in zip(bplot['boxes'], colors):
+		patch.set_facecolor(color)
+        
+f.subplots_adjust(hspace=0)
 plt.show()
 
-f2, a2 = plt.subplots(1,38, sharex=False, sharey=True)
+f, a = plt.subplots(1,40, sharex=False, sharey=True)
 
-for i in range(80, 119):
-    box_true = data[data['predicted'] == i][data['real'] == i]['teng']
-    box_false = data[data['predicted'] != i][data['real'] == i]['teng']
-    a2[i].boxplot([box_true, box_false], widths=0.8, labels=['+', '-'])
-    a2[i].set_title(i)
+for i in range(40):
+	j=i+40
+	box_true = data[data['predicted'] == j][data['real'] == j][var]
+	box_false = data[data['predicted'] != j][data['real'] == j][var]
+	print("loading", j)
+	if j != 63:
+		bplot = a[i].boxplot([box_true, box_false], widths=0.8, labels=['+', '-'],patch_artist=True)
+	a[i].set_title(j)
+
+	for patch, color in zip(bplot['boxes'], colors):
+		patch.set_facecolor(color)
+        
+f.subplots_adjust(hspace=0)
+plt.show()
+
+f, a = plt.subplots(1,38, sharex=False, sharey=True)
+
+for i in range(38):
+	j=i+80
+	box_true = data[data['predicted'] == j][data['real'] == j][var]
+	box_false = data[data['predicted'] != j][data['real'] == j][var]
+	print("loading", j)
+	bplot = a[i].boxplot([box_true, box_false], widths=0.8, labels=['+', '-'],patch_artist=True)
+	a[i].set_title(j)
+
+	for patch, color in zip(bplot['boxes'], colors):
+		patch.set_facecolor(color)
     
-f2.subplots_adjust(hspace=0)
+f.subplots_adjust(hspace=0)
 plt.show()
 """
