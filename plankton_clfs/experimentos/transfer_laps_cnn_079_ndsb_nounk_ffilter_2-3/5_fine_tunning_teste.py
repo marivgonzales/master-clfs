7e9aae0fc6da799e4126d8137980faa9e93115d5
Mariane Valerio Gonzales (MSc | hirata) <mariane@escience11.vision.ime.usp.br>
2019-06-08 22:47:56 -0300

    adding experiments run on the fixed code

--- plankton_clfs/experimentos/transfer_laps_cnn_079_ndsb_nounk_ffilter_2-3/5_fine_tunning_teste.py
+++ plankton_clfs/experimentos/transfer_laps_cnn_079_ndsb_nounk_ffilter_2-3/5_fine_tunning_teste.py
@@ -19,82 +19,6 @@ from skimage.transform import resize
 import cv2
 import os
 
-def TENG(img):
-    """Implements the Tenengrad (TENG) focus measure operator.
-    Based on the gradient of the image.
-    :param img: the image the measure is applied to
-    :type img: numpy.ndarray
-    :returns: numpy.float32 -- the degree of focus
-    """
-    gaussianX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
-    gaussianY = cv2.Sobel(img, cv2.CV_64F, 1, 0)
-    return np.mean(gaussianX * gaussianX +
-                      gaussianY * gaussianY)
-
-def PreprocessImgs(imgs, target_size):
-    new_imgs = np.ones((len(imgs), target_size[0], target_size[1]))
-
-    for i in range(len(imgs)):
-        current = imgs[i]
-        majorside = np.amax(current.shape)
-        majorside_idx = np.argmax(current.shape)
-        minorside = np.amin(current.shape)
-
-        factor = target_size[0]/majorside
-        minorside_new = floor(minorside*factor)
-        minorside_pad = floor((target_size[0] - minorside_new)/2)
-        
-        if majorside_idx == 0:
-            current = resize(current, (target_size[0], minorside_new), mode='constant', anti_aliasing=True)
-            for j in range(current.shape[0]):
-                for k in range(current.shape[1]):
-                    new_imgs[i,j,(k + minorside_pad)] = current[j,k]
-
-        if majorside_idx == 1:
-            current = resize(current, (minorside_new, target_size[1]), mode='constant', anti_aliasing=True)
-            for j in range(current.shape[0]):
-                for k in range(current.shape[1]):
-                    new_imgs[i,(j + minorside_pad),k] = current [j,k]
-
-        new_imgs[i] = new_imgs[i].astype('float32')
-
-    
-    return new_imgs
-
-def LoadTrainData(target_shape):
-    train_path = "../laps_nobg_100/images_train.npy.gz"
-    labels_path = "../laps_nobg_100/labels_train.npy.gz"
-    with gzip.open(labels_path, "rb") as f:
-        labels = np.load(f)
-
-    with gzip.open(train_path, "rb") as f:
-        imgs = np.load(f)
-    imgs = PreprocessImgs(imgs, (target_shape[0], target_shape[1]))
-
-    focused_images = np.zeros((0,target_shape[0], target_shape[1]))
-    focused_labels = np.zeros((0))
-
-    for i in range(len(imgs)):
-        focus_meas = TENG(imgs[i])
-        focus_measure = np.log(focus_meas)
-        if focus_measure > -2.3:
-            img = np.reshape(imgs[i], (1, target_shape[0], target_shape[1]))
-            focused_images = np.append(focused_images, img, axis=0)
-            focused_labels = np.append(focused_labels, labels[i])
-
-    new_shape = (len(focused_images), target_shape[0], target_shape[1], target_shape[2])
-    focused_images = np.reshape(focused_images, new_shape)
-
-    split = StratifiedShuffleSplit(n_splits=1,test_size=0.1)
-    train_idx, valid_idx = next(split.split(np.zeros(len(focused_labels)), focused_labels))
-
-    np.save("./train_indices_ftteste5.npy", train_idx)
-    np.save("./valid_indices_ftteste5.npy", valid_idx)
-
-    X_train, y_train = focused_images[train_idx], labels[train_idx]
-    X_valid, y_valid = focused_images[valid_idx], labels[valid_idx]
-
-    return X_train, y_train, X_valid, y_valid
 
 def LoadModel(in_shape, num_classes):
     # FIXME: 
@@ -150,21 +74,25 @@ def step_decay(epoch):
         return 5e-3
 
 batch_size = 32
-num_classes= 20
 
 img_shape = (95, 95, 1)
 
-X_train, y_train, X_valid, y_valid = LoadTrainData(img_shape)
+X_train = np.load("../laps_nobg_100/laps_images_train_ffilter_2-3.npy")
+y_train = np.load("../laps_nobg_100/laps_real_labels_train_ffilter_2-3.npy")
 
-print(np.shape(y_train))
-print(np.shape(y_valid))
+X_valid = np.load("../laps_nobg_100/laps_images_valid_ffilter_2-3.npy")
+y_valid = np.load("../laps_nobg_100/laps_real_labels_valid_ffilter_2-3.npy")
 
+
+print("Conjunto de treino: ", np.shape(y_train))
+print("Conjunto de validação: ", np.shape(y_valid))
+
+num_classes = 20
 
 X_train = X_train.astype("float32")
 X_valid = X_valid.astype("float32")
 y_train = to_categorical(y_train, num_classes)
 y_valid = to_categorical(y_valid, num_classes)
-
 
 datagen = ImageDataGenerator(
                              rotation_range=60,
@@ -243,15 +171,11 @@ predictions_valid = model.predict(X_valid, batch_size=batch_size)
 predicted_labels_valid = np.argmax(predictions_valid, axis=1)
 np.save("./complete_predictions_valid_ftteste5.npy", predictions_valid)
 np.save("./predicted_labels_valid_ftteste5.npy", predicted_labels_valid)
-np.save("./real_labels_valid_ftteste5.npy", y_valid)
-
 
 predictions = model.predict(X_train, batch_size=batch_size)
 predicted_labels = np.argmax(predictions, axis=1)
 np.save("./complete_predictions_train_ftteste5.npy", predictions)
 np.save("./predicted_labels_train_ftteste5.npy", predicted_labels)
-np.save("./real_labels_train_ftteste5.npy", y_train)
-
 print("Predictions saved")
 
 
